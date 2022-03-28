873e4ba546532e10e0d8bc83fde956297da81b27
Alexey Balakhanov <alexeybalahanov@gmail.com>
2022-03-28 23:12:35 +0300

    create clear Friends component

--- src/components/Friends/Friends.jsx
+++ src/components/Friends/Friends.jsx
@@ -1,66 +1,47 @@
-import React from 'react';
 import s from './Friends.module.css';
 import Friend from './Friend/Friend'
 import User from './Users/User'
-import * as axios from 'axios';
 
-class Friends extends React.Component {
-    constructor(props) {
-        super(props);
-        document.title = 'Friends';
-    }
+const Friends = (props) => {
+    const pages = (num) => {
+        let pages = [];
+        for(let i = 1; i <= num; i++){
+            pages.push(<span onClick={(e) => props.setPage(e)} className={(i == props.currentPage ? s.active : s.disabled)}>{i}</span>)
+        }
+        return pages
+    } 
 
-
-    friends = () => {
-        return ([...this.props.friends]
+    const friends = () => {
+        return ([...props.friends]
         .map(friend => {
-            return <Friend follow={this.props.follow} unfollow={this.props.unfollow} friend={friend}/>
+            return <Friend follow={props.follow} unfollow={props.unfollow} friend={friend}/>
         }))
     } 
 
-    componentDidMount() {
-        axios.get("https://social-network.samuraijs.com/api/1.0/users?count=5&page=1").then(response => response.data.items).then(response => {this.props.setUsers(response)});
-    }
-
-    users = () => {
-        return ([...this.props.users.users])
+    
+    const users = () => {
+        return ([...props.users])
         .map(user => {
-            return <User follow={this.props.follow} unfollow={this.props.unfollow} user={user}/>
+            return <User follow={props.follow} unfollow={props.unfollow} user={user}/>
         })
     }
 
-    setPage = (event) => {
-        let page = +event.target.textContent;
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?count=5&page=${page}`).then(response => response.data.items).then(response => {
-            this.props.setPage(page);
-            this.props.setUsers(response);
-        });
-    }
-
-    pages = (num) => {
-        let pages = [];
-        for(let i = 1; i <= num; i++){
-            pages.push(<span onClick={(e) => this.setPage(e)} className={(i == this.props.currentPage ? s.active : s.disabled)}>{i}</span>)
-        }
-        return pages
-    }
-
-    render() {
-        return (<main className={s.friends}>
+    return (
+        <main className={s.friends}>
             <div className={s.friendsInfo}>
                 <h3>Your Friends:</h3>
             </div>
-            {this.friends()}
+            {friends()}
             <div className={s.usersInfo}>
                 <h3>Find Friends:</h3>
                 <div className={s.pages}>
-                    {this.pages(this.props.pageCount)}
+                    {pages(props.pageCount)}
                     <span>...</span>
                 </div>
             </div>
-            {this.users()}
-        </main>)
-    }
+            {users()}
+        </main>
+    )
 }
 
 export default Friends;
--- src/components/Friends/FriendsContainer.jsx
+++ src/components/Friends/FriendsContainer.jsx
@@ -1,9 +1,42 @@
+import React from 'react';
 import Friends from './Friends';
+import * as axios from 'axios';
 import { followActionCreater } from "../../redux/friends-reducer";
 import { unfollowActionCreater } from "../../redux/friends-reducer";
 import { setUsersActionCreater } from "../../redux/users-reducer";
 import { setCurrentPageActionCreater } from "../../redux/users-reducer";
 import { connect } from 'react-redux';
+
+class FriendsContainer extends React.Component {
+    constructor(props) {
+        super(props);
+        document.title = 'Friends';
+    }
+
+    componentDidMount() {
+        axios.get("https://social-network.samuraijs.com/api/1.0/users?count=5&page=1").then(response => response.data.items).then(response => {this.props.setUsers(response)});
+    }
+
+    setPage = (event) => {
+        let page = +event.target.textContent;
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?count=5&page=${page}`).then(response => response.data.items).then(response => {
+            this.props.setPage(page);
+            this.props.setUsers(response);
+        });
+    }
+
+    render() {
+        return (<Friends 
+            pageCount={this.props.pageCount}
+            currentPage={this.props.currentPage}
+            follow={this.props.follow}
+            unfollow={this.props.unfollow}
+            friends={this.props.friends}
+            users={this.props.users.users}
+            setPage={this.setPage}
+        />)
+    }
+}
 
 const mapStateToProps = (state) => {
     return {
@@ -32,6 +65,4 @@ const mapDispatchToProps = (dispatch) => {
 }
 
 
-const FriendsContainer = connect(mapStateToProps, mapDispatchToProps)(Friends);
-
-export default FriendsContainer;
+export default connect(mapStateToProps, mapDispatchToProps)(FriendsContainer);
